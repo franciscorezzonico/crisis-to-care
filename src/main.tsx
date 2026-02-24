4691a9c678bce4e00b8264570bf8e6ad6b01df03
Francisco Agustín Rezzónico <rezzonicofrancisco@gmail.com>
2026-02-24 18:39:30 -0500

    Changes to React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './contexts/LanguageContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+     </BrowserRouter> 
   </StrictMode>,
 );
 
